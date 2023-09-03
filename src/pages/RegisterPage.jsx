742d07f0a40f7eade0299e0d875aa0d96ef3d269
Muhammad Rafai <rafaimhd123@gmail.com>
2023-09-03 14:19:41 +0700

    add logic for register and check if the email was registered

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,60 @@
 import { Container } from "react-bootstrap"
 import loginImage from "./../assets/login.svg"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import Swal from "sweetalert2"
 
 const RegisterPage = () => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmitRegister = (e) => {
+        e.preventDefault()
+        fetch(`https://64f2052d0e1e60602d24967d.mockapi.io/admins?email=${email}`)
+        .then((response) => response.json())
+        .then((result) => {
+            if (result.length === 0) {
+                fetch("https://64f2052d0e1e60602d24967d.mockapi.io/admins", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        name: name,
+                        email: email,
+                        password: password,
+                    }),
+                })
+                .then((response) => response.json())
+                .then((result) => {
+                    // console.log("Success:", result);
+                    // alert("Register berhasil, silahkan login")
+                    new Swal(
+                        "Success!",
+                        "Akun anda berhasil di daftarkan, Silahkan Login.",
+                        "success",
+                        {
+                            timer: 3000,
+                        },
+                        navigate("/login")
+                    );
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+            } else {
+                new Swal("Email Sudah Terdaftar!", "Silahkan gunakan email lain.", "warning", {
+                    timer: 3000,
+                });
+            }
+        })
+    }        
+
+
+
     return (
         <>
             <Container>
@@ -10,18 +62,18 @@ const RegisterPage = () => {
                     <div className="col-md-6 d-flex flex-column  justify-content-center  " style={{minHeight: "80vh"}}>
                         <h1>Selamat Datang Di Absensi STIKOM</h1>
                         <p className="lead">Kamu belum daftar? Daftar yuk!</p>
-                        <form>
+                        <form onSubmit={handleSubmitRegister}>
                             <div class="mb-3">
                                 <label for="name-register" class="form-label">Name</label>
-                                <input type="text" class="form-control" id="name-register" aria-describedby="emailHelp" placeholder="Silahkan masukkan nama anda"  />
+                                <input value={name} onChange={e => setName(e.target.value)} type="text" class="form-control" id="name-register" aria-describedby="emailHelp" placeholder="Silahkan masukkan nama anda"  />
                             </div>
                             <div class="mb-3">
                                 <label for="email-register" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="email-register" aria-describedby="emailHelp" placeholder="Silahkan masukkan email anda" />
+                                <input value={email} onChange={e => setEmail(e.target.value)} type="email" class="form-control" id="email-register" aria-describedby="emailHelp" placeholder="Silahkan masukkan email anda" />
                             </div>
                             <div class="mb-3">
                                 <label for="password-register" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password-register" placeholder="Masukkan password anda" />
+                                <input value={password} onChange={e => setPassword(e.target.value)} type="password" class="form-control" id="password-register" placeholder="Masukkan password anda" />
                             </div>
                             <div class="mb-3">
                                 <p>Sudah punya akun? silahkan <Link to="/login" className="fw-bold text-purple-500">Login </Link> yuk! </p>
