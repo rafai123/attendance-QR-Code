947db68c809fd347b731562665f8e382098b8c85
Muhammad Rafai <rafaimhd123@gmail.com>
2023-09-06 20:32:13 +0700

    add _redirects file

---
+++ src/pages/ScanRoomPage.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap"
+
+const ScanRoomPage = () => {
+    return (
+        <>
+            <Container className="text-white">
+                <h1>Scan Room Page</h1>
+            </Container>
+        </>
+    )
+}
+
+export default ScanRoomPage
